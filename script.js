9bc2f9d84d2d88b0956890f878143e3885d098d6
20R01A05C5 <123346414+20R01A05C5@users.noreply.github.com>
2023-08-12 14:15:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-let hr = document.getElementById('hour');
-let min = document.getElementById('min');
-let sec = document.getElementById('sec');
+var second = 0;
+var minute = 0;
+var hour = 0;
+var d = new Date();
 
-function displayTime(){
-    let date = new Date();
-
-    // Getting hour, mins, secs from date
-    let hh = date.getHours();
-    let mm = date.getMinutes();
-    let ss = date.getSeconds();
-
-    let hRotation = 30*hh + mm/2;
-    let mRotation = 6*mm;
-    let sRotation = 6*ss;
-
-    hr.style.transform = `rotate(${hRotation}deg)`;
-    min.style.transform = `rotate(${mRotation}deg)`;
-    sec.style.transform = `rotate(${sRotation}deg)`;
-
-}
-
-setInterval(displayTime, 1000);
+setInterval(function () {
+  d = new Date();
+  second = d.getSeconds() * 6;
+  minute = d.getMinutes() * 6;
+  hour = d.getHours() * 30 + Math.round(minute / 12);
+  document.getElementById("second-hand").style.transform =
+    "rotate(" + second + "deg)";
+  document.getElementById("minute-hand").style.transform =
+    "rotate(" + minute + "deg)";
+  document.getElementById("hour-hand").style.transform =
+    "rotate(" + hour + "deg)";
+}, 1000);
